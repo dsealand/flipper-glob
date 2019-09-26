be7de6a8defc6a519ee4f1737106cf599ea6536c
hmc-catfish-f18 <aroeseler@g.hmc.edu>
2019-09-26 12:52:56 -0700

    implemented loading initial value from database

--- flipper-glob-webpage/src/App.js
+++ flipper-glob-webpage/src/App.js
@@ -2,38 +2,55 @@ import React, { Component } from 'react';
 import './App.css';
 import firebase from './firebase.js';
 
+
 class App extends Component {
   constructor() {
     super();
     this.state = {
+      //population counter, initially set to 0
       currentCount: 0
     }
-    this.handleIncrement = this.handleIncrement.bind(this);
-    this.handleDecrement = this.handleDecrement.bind(this);
+
+    //binds the handleSubmit() function to all buttons to update the databse
+    //on each click of every button
     this.handleSubmit = this.handleSubmit.bind(this);
   }
-  
+
   handleIncrement() {
-    this.setState({
-      currentCount: this.state.currentCount + 1
-    });
+    this.setState({ currentCount: this.state.currentCount + 1 });
   }
 
   handleDecrement() {
-    this.setState({
-      currentCount: this.state.currentCount - 1
-    });
+    this.setState({ currentCount: this.state.currentCount - 1 });
 }
+  
+  handleReset() {
+    this.setState({ currentCount: 0 });
+  }
 
   handleSubmit(e) {
+    //Stops the page from reloading every click
     e.preventDefault();
+    //connects to firebase
     const itemsRef = firebase.database().ref('count');
     const item = {
       value: this.state.currentCount
     }
-    //itemsRef.push(item);
+    //updates existing item on firebase or creates new item if one does not exist
     itemsRef.update(item);
 }
+
+// Automatically called by React everytime a component is mounted (loaded)
+// on to the webpage
+  componentDidMount() {
+    // connects to the database
+    const database = firebase.database();
+    // pulls data once from the section titled "count" and its child titled "value"
+    database.ref('count').once('value', (snapshot) => {
+    // writes that value to currentCount
+    this.setState({ currentCount: snapshot.val().value });
+  });
+  }
 
   render() {
     return (
@@ -41,16 +58,19 @@ class App extends Component {
         <header>
             <div className='wrapper'>
               <h1>Flipper Glob</h1>
-              
             </div>
         </header>
         <div className='container'>
           <section className='display-count'>
               <form onSubmit={this.handleSubmit}>
                 <h3>The number of people in the Hoch is:</h3>
+                {/* loads the value of currentCount */}
                 <h1>{this.state.currentCount}</h1>
-                <button id="increment" onClick={() => this.handleIncrement()}>Increment</button>
-                <button id="decrement" onClick={() => this.handleDecrement()}>Decrement</button>
+                {/* Assigns a function to be called on the button press IN ADDITION
+                    to handleSubmit */}
+                <button onClick={() => this.handleIncrement()}>Increment</button>
+                <button onClick={() => this.handleDecrement()}>Decrement</button>
+                <button onClick={() => this.handleReset()}>Reset</button>
               </form>
           </section>
           <section className='display-item'>
@@ -64,4 +84,5 @@ class App extends Component {
     );
   }
 }
+
 export default App;
