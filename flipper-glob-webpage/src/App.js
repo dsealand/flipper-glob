834b0a1b9aa5064524403aa903f91b74ed4bae88
hmc-catfish-f18 <aroeseler@g.hmc.edu>
2019-09-25 21:40:32 -0700

    basic increment and decrement working

--- flipper-glob-webpage/src/App.js
+++ flipper-glob-webpage/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import firebase from './firebase.js';
 
 class App extends Component {
   constructor() {
@@ -8,9 +8,33 @@ class App extends Component {
     this.state = {
       currentCount: 0
     }
+    this.handleIncrement = this.handleIncrement.bind(this);
+    this.handleDecrement = this.handleDecrement.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
   
-  
+  handleIncrement() {
+    this.setState({
+      currentCount: this.state.currentCount + 1
+    });
+  }
+
+  handleDecrement() {
+    this.setState({
+      currentCount: this.state.currentCount - 1
+    });
+}
+
+  handleSubmit(e) {
+    e.preventDefault();
+    const itemsRef = firebase.database().ref('count');
+    const item = {
+      value: this.state.currentCount
+    }
+    //itemsRef.push(item);
+    itemsRef.update(item);
+}
+
   render() {
     return (
       <div className='app'>
@@ -22,11 +46,11 @@ class App extends Component {
         </header>
         <div className='container'>
           <section className='display-count'>
-              <form>
+              <form onSubmit={this.handleSubmit}>
                 <h3>The number of people in the Hoch is:</h3>
                 <h1>{this.state.currentCount}</h1>
-                <button>Increment</button>
-                <button>Decrement</button>
+                <button id="increment" onClick={() => this.handleIncrement()}>Increment</button>
+                <button id="decrement" onClick={() => this.handleDecrement()}>Decrement</button>
               </form>
           </section>
           <section className='display-item'>
