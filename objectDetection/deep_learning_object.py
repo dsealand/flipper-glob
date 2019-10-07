4721f705fa44054027783b451c83192edf3d122c
dsealand <daniel.c.sealand@gmail.com>
2019-10-06 19:03:16 -0700

    loaded files, created blob, and passed into neural net

--- objectDetection/deep_learning_object.py
+++ objectDetection/deep_learning_object.py
@@ -14,3 +14,21 @@ ap.add_argument("-m", "--model", required=TRUE, help="path to pretrained Caffe m
 ap.add_argument("-c", "--conficence", type=float, default = 0.2, help="minimum probability to filter weak detections")
 args = vars(ap.parse_args())
 
+# define classification labels that MobileNet SSD was trained on
+CLASSES = ["background", "aeroplane", "bicycle", "bird", "boat", "bottle", "bus", "car", "cat", "chair", "cow", "diningtable",
+           "dog", "horse", "motorbike", "person", "pottedplant", "sheep", "sofa", "train", "tvmonitor"]
+COLORS = np.random.uniform(0, 255, size=(len(CLASSES), 3))
+
+# load model from files
+print("loading model")
+net = cv2.dnn.readNetFromCaffe(args["prototxt"], args["model"])
+
+# load image from file and prepare blob
+image = cv2.imread(args["image"])
+(h, w) = image.shape[:2]
+blob = cv2.dnn.blobFromImage(cv2.resize(image, (300, 300)), 0.007843, (300, 300), 127.5)
+
+# pass blob through neural net and get classifications/predictions
+print("detecting objects")
+net.setInput(blob)
+detections = net.forward()
