82f8d9e1237cc29f311b37e479d3d3edbfa13883
dsealand <daniel.c.sealand@gmail.com>
2019-10-06 19:16:24 -0700

    added test images and displayed object detection

--- objectDetection/deep_learning_object.py
+++ objectDetection/deep_learning_object.py
@@ -8,10 +8,10 @@ import cv2
 
 # define arguments when running file
 ap = argparse.ArgumentParser()
-ap.add_argument("-i", "--image", required=TRUE, help="path to image file")
-ap.add_argument("-p", "--prototxt", required=TRUE, help="path to Caffe 'deploy' prototxt file")
-ap.add_argument("-m", "--model", required=TRUE, help="path to pretrained Caffe model")
-ap.add_argument("-c", "--conficence", type=float, default = 0.2, help="minimum probability to filter weak detections")
+ap.add_argument("-i", "--image", required=True, help="path to image file")
+ap.add_argument("-p", "--prototxt", required=True, help="path to Caffe 'deploy' prototxt file")
+ap.add_argument("-m", "--model", required=True, help="path to pretrained Caffe model")
+ap.add_argument("-c", "--confidence", type=float, default = 0.2, help="minimum probability to filter weak detections")
 args = vars(ap.parse_args())
 
 # define classification labels that MobileNet SSD was trained on
@@ -32,3 +32,26 @@ blob = cv2.dnn.blobFromImage(cv2.resize(image, (300, 300)), 0.007843, (300, 300)
 print("detecting objects")
 net.setInput(blob)
 detections = net.forward()
+
+# loop over the detections
+for i in np.arange(0, detections.shape[2]):
+    # extract the confidence (i.e., probability) associated with prediction
+    confidence = detections[0, 0, i, 2]
+
+    # filter out weak detections by ensuring the `confidence` greater than the minimum confidence
+    if confidence > args["confidence"]:
+        # extract the index of the class label from the then compute the (x, y)-coordinates of the bounding box the object
+        idx = int(detections[0, 0, i, 1])
+        box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
+        (startX, startY, endX, endY) = box.astype("int")
+
+        # display the prediction
+        label = "{}: {:.2f}%".format(CLASSES[idx], confidence * 100)
+        print("[INFO] {}".format(label))
+        cv2.rectangle(image, (startX, startY), (endX, endY), COLORS[idx], 2)
+        y = startY - 15 if startY - 15 > 15 else startY + 15
+        cv2.putText(image, label, (startX, y), cv2.FONT_HERSHEY_SIMPLEX, 0.5, COLORS[idx], 2)
+
+# show the output image
+cv2.imshow("Output", image)
+cv2.waitKey(0)
