43d74cee9daf23ed4c1030055dc5584c65adec86
dsealand <daniel.c.sealand@gmail.com>
2019-10-08 12:39:35 -0700

    processed video stream instead of image file

--- objectDetection/deep_learning_object.py
+++ objectDetection/deep_learning_object.py
@@ -5,6 +5,9 @@
 import numpy as np
 import argparse
 import cv2
+from imutils.video import VideoStream
+import imutils
+import time
 
 # define arguments when running file
 ap = argparse.ArgumentParser()
@@ -23,35 +26,46 @@ COLORS = np.random.uniform(0, 255, size=(len(CLASSES), 3))
 print("loading model")
 net = cv2.dnn.readNetFromCaffe(args["prototxt"], args["model"])
 
-# load image from file and prepare blob
-image = cv2.imread(args["image"])
-(h, w) = image.shape[:2]
-blob = cv2.dnn.blobFromImage(cv2.resize(image, (300, 300)), 0.007843, (300, 300), 127.5)
+# create video stream from camera
+stream = VideoStream(src=0).start()
+time.sleep(2.0)
 
-# pass blob through neural net and get classifications/predictions
-print("detecting objects")
-net.setInput(blob)
-detections = net.forward()
+# display video stream
+while True:
+    image = stream.read()
+    image = imutils.resize(image, width=300)
+    
+    #image = cv2.imread(args["image"])
+    (h, w) = image.shape[:2]
+    blob = cv2.dnn.blobFromImage(cv2.resize(image, (300, 300)), 0.007843, (300, 300), 127.5)
+    
+    net.setInput(blob)
+    detections = net.forward()
+    
+    # loop over the detections
+    for i in np.arange(0, detections.shape[2]):
+        # extract the confidence (i.e., probability) associated with prediction
+        confidence = detections[0, 0, i, 2]
 
-# loop over the detections
-for i in np.arange(0, detections.shape[2]):
-    # extract the confidence (i.e., probability) associated with prediction
-    confidence = detections[0, 0, i, 2]
+        # filter out weak detections by ensuring the `confidence` greater than the minimum confidence
+        if confidence > args["confidence"]:
+            # extract the index of the class label from the then compute the (x, y)-coordinates of the bounding box the object
+            idx = int(detections[0, 0, i, 1])
+            box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
+            (startX, startY, endX, endY) = box.astype("int")
+            centroid = ((endX - startX)/2, (endY - startY)/2)
 
-    # filter out weak detections by ensuring the `confidence` greater than the minimum confidence
-    if confidence > args["confidence"]:
-        # extract the index of the class label from the then compute the (x, y)-coordinates of the bounding box the object
-        idx = int(detections[0, 0, i, 1])
-        box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
-        (startX, startY, endX, endY) = box.astype("int")
-
-        # display the prediction
-        label = "{}: {:.2f}%".format(CLASSES[idx], confidence * 100)
-        print("[INFO] {}".format(label))
-        cv2.rectangle(image, (startX, startY), (endX, endY), COLORS[idx], 2)
-        y = startY - 15 if startY - 15 > 15 else startY + 15
-        cv2.putText(image, label, (startX, y), cv2.FONT_HERSHEY_SIMPLEX, 0.5, COLORS[idx], 2)
-
-# show the output image
-cv2.imshow("Output", image)
-cv2.waitKey(0)
+            # display the prediction
+            label = "{}: {:.2f}%".format(CLASSES[idx], confidence * 100)
+            cv2.rectangle(image, (startX, startY), (endX, endY), COLORS[idx], 2)
+            y = startY - 15 if startY - 15 > 15 else startY + 15
+            cv2.putText(image, label, (startX, y), cv2.FONT_HERSHEY_SIMPLEX, 0.5, COLORS[idx], 2)
+        
+        
+    cv2.imshow("frame", image)
+    key = cv2.waitKey(1) & 0xFF
+    if key == ord("q"):
+        break
+    
+cv2.destroyAllWindows()
+stream.stop()
