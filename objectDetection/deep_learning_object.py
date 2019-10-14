28d14df08a8609c22b88095d56988b5ed03e96d3
dsealand <daniel.c.sealand@gmail.com>
2019-10-13 21:45:18 -0700

    removed test image arg processing

--- objectDetection/deep_learning_object.py
+++ objectDetection/deep_learning_object.py
@@ -11,7 +11,6 @@ import time
 
 # define arguments when running file
 ap = argparse.ArgumentParser()
-ap.add_argument("-i", "--image", required=True, help="path to image file")
 ap.add_argument("-p", "--prototxt", required=True, help="path to Caffe 'deploy' prototxt file")
 ap.add_argument("-m", "--model", required=True, help="path to pretrained Caffe model")
 ap.add_argument("-c", "--confidence", type=float, default = 0.2, help="minimum probability to filter weak detections")
